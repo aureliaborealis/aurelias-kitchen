c3e95c468eeb5d71747ff8e9319423e1776b9fc5
aureliaborealis <svetla.kokalova@gmail.com>
2017-01-31 22:42:42 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -19,10 +19,9 @@
 
   function getQueryVariable(variable) {
     var query = decodeURIComponent(window.location.search.substring(1));
-    var vars = query.split('&');
 
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
+    for (var i = 0; i < query.length; i++) {
+      var pair = query[i].split('=');
 
       if (pair[0] === variable) {
         return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
