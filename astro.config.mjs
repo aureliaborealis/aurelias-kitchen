9703f180eddb03779d955f1d3320899fc0fddeea
Alexander Futekov <futekov@gmail.com>
2026-07-28 23:41:32 +0300

    config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://aurelias-kitchen.com',
 	integrations: [mdx(), sitemap(), pagefind()],
 	fonts: [
 		{
